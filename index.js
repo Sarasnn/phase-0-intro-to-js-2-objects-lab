301cb3ef39ab9267d274a85afd1810799b52e588
Sara Salguero <Sara_salgueronavarro@hotmail.com>
2022-09-14 17:33:38 -0400

    commit tree dc633d1b9210fe3654edd3faf356cee98748322e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sara",
+    streetAddress: "calle san pedro",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj,
+    [key]: value,
+};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
